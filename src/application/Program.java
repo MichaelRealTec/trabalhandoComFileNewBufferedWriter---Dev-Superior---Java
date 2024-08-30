1add4051bde1e9b4aad95cfef504fd6353939e1b
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-08-29 23:23:34 -0300

    Trabalhando com BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,24 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// trabalhandoComFileNewBufferedWriter
-
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"}; // Instanciar vetor com 3 valor do tipo String
+		String path = "c:\\temp\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
